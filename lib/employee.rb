b70a49620fc21b0430e5bb566898116ac23ca78c
sweetmangoes <alphonsechristian@gmail.com>
2023-03-13 21:36:44 -0400

    done ex7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: {only_integer: true, greater_than: 39, less_than: 201 }
+  validates_associated :store
 end
